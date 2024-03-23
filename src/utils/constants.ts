1cdb86d12e035896152667fdee5e1cf3510a11fe
Nora Liu <noraliu0830@gmail.com>
2024-03-23 15:16:06 +0800

    feat: add constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,15 @@
 export const APP_SESSION = "APP_SESSION";
 export const DEFAULT_NETWORK = "KUSAMA";
 export const NETWORK = "network";
+export const ROCOCO = "wss://rococo-rpc.polkadot.io";
+export const WESTEND = "wss://westend-rpc.polkadot.io";
+export const KUSAMA = "wss://kusama-rpc.polkadot.io";
+export const POLKADOT = "wss://rpc.polkadot.io";
+export const KABOCHA = "wss://kabocha.jelliedowl.net";
+export const NODE_ENDPOINT = {
+  ROCOCO,
+  WESTEND,
+  KUSAMA,
+  POLKADOT,
+  KABOCHA,
+};
