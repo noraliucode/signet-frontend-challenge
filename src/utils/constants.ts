83c62cc051268764b84b8d83ac708885a48c528e
Nora Liu <noraliu0830@gmail.com>
2024-03-26 13:07:42 +0800

    feat: add shibuya testnet

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,7 @@ export const WESTEND = "wss://westend-rpc.polkadot.io";
 export const KUSAMA = "wss://kusama-rpc.polkadot.io";
 export const POLKADOT = "wss://rpc.polkadot.io";
 export const KABOCHA = "wss://kabocha.jelliedowl.net";
+export const SHIBUYA = "wss://rpc.shibuya.astar.network";
 export const PURE_CREATED = "PureCreated";
 const assetPath = "/assets/networks/";
 export const NODE_ENDPOINT = {
@@ -14,6 +15,7 @@ export const NODE_ENDPOINT = {
   KUSAMA,
   POLKADOT,
   KABOCHA,
+  SHIBUYA,
 };
 export const DECIMALS = {
   ROCOCO: 12,
@@ -21,10 +23,12 @@ export const DECIMALS = {
   KUSAMA: 12,
   KABOCHA: 12,
   POLKADOT: 10,
+  SHIBUYA: 18,
 };
 export const NETWORKS = [
   // { network: "KUSAMA", icon: `${assetPath}kusama.gif` },
   // { network: "POLKADOT", icon: `${assetPath}polkadot.svg` },
   { network: "ROCOCO", icon: `${assetPath}rococo.svg` },
-  { network: "WESTEND", icon: `${assetPath}westend.svg` },
+  // { network: "WESTEND", icon: `${assetPath}westend.svg` },
+  { network: "SHIBUYA", icon: `${assetPath}shibuya.png` },
 ];
